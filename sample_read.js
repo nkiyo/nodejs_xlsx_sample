75c6a07c1e0ff7bcf390ba9a2b258de9dd3f0e5c
nkiyo <nkiyo@hoge.com>
2019-09-28 08:29:07 +0900

    Apply eslint

--- sample_read.js
+++ sample_read.js
@@ -3,12 +3,13 @@
 // https://stackoverflow.com/a/51442854
 // https://qiita.com/indometacin/items/020513f7801a040dab33
 
-const xlsx = require('xlsx');
-const utils = xlsx.utils;
+const xlsx = require("xlsx");
+
+const { utils } = xlsx;
 
 // 抽出条件の指定
 // -  Excelファイルパス
-const xlsxFile = 'sample.xlsx';
+const xlsxFile = "sample.xlsx";
 // -  Excelシート名
 const targetSheet = "hogesheet";
 // - 抽出したいデータ行のID(ラベル文字列)
@@ -16,46 +17,45 @@ const targetLineId = "3";
 
 // Excelファイル内の全シートをループで回す
 const book = xlsx.readFile(xlsxFile);
-for(const name of book.SheetNames) {
-  if(name !== targetSheet) {
+for (const name of book.SheetNames) {
+  if (name !== targetSheet) {
     continue;
   }
   console.log(`Reading a sheet "${name}" in a book "${xlsxFile}".`);
 
   // データが存在する範囲のセルをループで回す
   const sheet = book.Sheets[name];
-  let range = sheet['!ref'];
+  const range = sheet["!ref"];
   const rangeVal = utils.decode_range(range);
   const keys = [];
   const vals = [];
-  for(let r = rangeVal.s.r; r <= rangeVal.e.r; r++) {
-    for(let c = rangeVal.s.c; c <= rangeVal.e.c; c++) {
-      const adr = utils.encode_cell({c:c, r:r});
+  for (let { r } = rangeVal.s; r <= rangeVal.e.r; r++) {
+    for (let { c } = rangeVal.s; c <= rangeVal.e.c; c++) {
+      const adr = utils.encode_cell({ c, r });
       const cell = sheet[adr];
 
       // ラベル行(2行目)の値を保存
-      if(/^[A-Z]+2$/.test(adr)) {
+      if (/^[A-Z]+2$/.test(adr)) {
         keys.push(cell.v);
       }
 
       // 対象データ行の値を保存
-      const adr2 = utils.encode_cell({c:1, r:r});
+      const adr2 = utils.encode_cell({ c: 1, r });
       const cell2 = sheet[adr2];
       let lineId = "";
-      if(typeof cell2 !== "undefined") {
+      if (typeof cell2 !== "undefined") {
         lineId = cell2.v.toString();
       }
-      if(lineId === targetLineId) {
+      if (lineId === targetLineId) {
         vals.push(cell.v);
       }
 
       // for debug
-      //if(typeof cell !== "undefined") {
+      // if(typeof cell !== "undefined") {
       //  console.log(`${adr} value:${cell.v}`);
-      //}
+      // }
     }
   }
   console.log(`keys are ${keys.map(k => `"${k}"`)}.`);
   console.log(`target val are ${vals.map(v => `"${v}"`)}.`);
 }
-
